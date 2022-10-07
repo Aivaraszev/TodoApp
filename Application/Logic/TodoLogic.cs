98d09cc3c3d2fde4ad84352d626472b9d8ddfe73
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-07 16:46:25 +0200

    Initial commit

---
+++ Application/Logic/TodoLogic.cs
@@ -0,0 +1,101 @@
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.Logic;
+
+public class TodoLogic : ITodoLogic
+{
+    private readonly ITodoDao _todoDao;
+    private readonly IUserDao _userDao;
+
+    public TodoLogic(ITodoDao todoDao, IUserDao userDao)
+    {
+        _todoDao = todoDao;
+        _userDao = userDao;
+    }
+
+    public async Task<Todo> CreateAsync(TodoCreationDto dto)
+    {
+        User? user = await _userDao.getByIdAsync(dto.OwnerId);
+        if (user == null)
+        {
+            throw new Exception($"User with id {dto.OwnerId} was not found.");
+        }
+
+        Todo todo = new Todo(user, dto.Title);
+        ValidateTodo(todo);
+        Todo created = await _todoDao.CreateAsync(todo);
+        return created;
+    }
+
+    public Task<IEnumerable<Todo>> GetAsync(SearchTodoParametersDto searchParameters)
+    {
+        return _todoDao.GetAsync(searchParameters);
+    }
+
+    public async Task UpdateAsync(TodoUpdateDto dto)
+    {
+        Todo? existing = await _todoDao.GetByIdAsync(dto.Id);
+        if (existing == null)
+        {
+            throw new Exception($"Todo with id {dto.Id} was not found.");
+        }
+
+        User? user = null;
+        if (dto.OwnerId != null)
+        {
+            user = await _userDao.getByIdAsync((int) dto.OwnerId);
+            if (user == null)
+            {
+                throw new Exception($"User with id {dto.OwnerId} was not found.");
+            }
+        }
+
+        if (dto.IsCompleted != null && existing.IsCompleted && !(bool) dto.IsCompleted)
+        {
+            throw new Exception("Todo cannot be un-completed");
+        }
+
+        User userToUse = user ?? existing.Owner;
+        string titleToUse = dto.Title ?? existing.Title;
+        bool completedToUse = dto.IsCompleted ?? existing.IsCompleted;
+
+        Todo updated = new(userToUse, titleToUse)
+        {
+            IsCompleted = completedToUse,
+            Id = existing.Id,
+        };
+
+        ValidateTodo(updated);
+
+        await _todoDao.UpdateAsync(updated);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        Todo? todo = await _todoDao.GetByIdAsync(id);
+        if (todo == null)
+        {
+            throw new Exception($"Todo with id {id} was not found");
+        }
+
+        if (!todo.IsCompleted)
+        {
+            throw new Exception($"Cannot delete not completed todo");
+        }
+
+        await _todoDao.DeleteAsync(id);
+    }
+
+    public async Task<Todo> GetByIdAsync(int id)
+    {
+        return await _todoDao.GetByIdAsync(id) ?? throw new Exception($"Todo with id {id} was not found");
+    }
+
+    private void ValidateTodo(Todo dto)
+    {
+        if (string.IsNullOrEmpty(dto.Title)) throw new Exception("Title cannot be empty.");
+    }
+}
---
+++ FileData/DAOs/TodoFileDao.cs
@@ -0,0 +1,88 @@
+﻿using Application.DaoInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+
+namespace FileData.DAOs;
+
+public class TodoFileDao : ITodoDao
+{
+    private readonly FileContext _context;
+
+    public TodoFileDao(FileContext context)
+    {
+        _context = context;
+    }
+
+    public Task<Todo> CreateAsync(Todo todo)
+    {
+        int id = 1;
+        if (_context.Todos.Any())
+        {
+            id = _context.Todos.Max(t => t.Id);
+            id++;
+        }
+
+        todo.Id = id;
+
+        _context.Todos.Add(todo);
+        _context.SaveChanges();
+
+        return Task.FromResult(todo);
+    }
+
+    public Task<IEnumerable<Todo>> GetAsync(SearchTodoParametersDto searchParameters)
+    {
+        IEnumerable<Todo> todos = _context.Todos.AsEnumerable();
+        if (!string.IsNullOrEmpty(searchParameters.Username))
+        {
+            todos = _context.Todos.Where(t =>
+                t.Owner.UserName.Contains(searchParameters.Username, StringComparison.OrdinalIgnoreCase));
+        }
+
+        if (!string.IsNullOrEmpty(searchParameters.TitleContains))
+        {
+            todos = todos.Where(t =>
+                t.Title.Contains(searchParameters.TitleContains, StringComparison.OrdinalIgnoreCase));
+        }
+
+        if (searchParameters.UserId != null)
+        {
+            todos = todos.Where(t =>
+                t.Owner.Id == searchParameters.UserId);
+        }
+
+        if (searchParameters.CompletedStatus != null)
+        {
+            todos = todos.Where(t =>
+                t.IsCompleted == searchParameters.CompletedStatus
+            );
+        }
+
+        return Task.FromResult(todos);
+    }
+
+    public Task UpdateAsync(Todo todo)
+    {
+        Todo? existing = _context.Todos.FirstOrDefault(t => t.Id == todo.Id);
+
+        _context.Todos.Remove(existing ?? throw new Exception($"Todo with id {todo.Id} does not exist"));
+
+        _context.Todos.Add(todo);
+        _context.SaveChanges();
+        return Task.CompletedTask;
+    }
+
+    public Task<Todo?> GetByIdAsync(int id)
+    {
+        Todo? existing = _context.Todos.FirstOrDefault(t => t.Id == id);
+        return Task.FromResult(existing);
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Todo? existing = _context.Todos.FirstOrDefault(t => t.Id == id);
+        _context.Todos.Remove(existing ?? throw new Exception($"Todo with id {id} does not exist"));
+        _context.SaveChanges();
+        return Task.CompletedTask;
+    }
+}
