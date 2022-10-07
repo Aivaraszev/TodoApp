3f91a9c741d14041f3f20a4e0d499993212eff63
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-07 21:24:05 +0200

    Add user service interface

--- HttpClients/ClientInterfaces/IUserService.cs
+++ HttpClients/ClientInterfaces/IUserService.cs
@@ -6,5 +6,7 @@ namespace HttpClients.ClientInterfaces;
 public interface IUserService
 {
    Task<User> CreateAsync(UserCreationDto dto);
-   
+
+   Task<IEnumerable<User>> GetUsersAsync(string? usernameContains = null);
+
 }
